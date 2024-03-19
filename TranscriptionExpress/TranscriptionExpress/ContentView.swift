a0ee59aefc1cde54692c6c796de29867accb0ab6
老房东 <huangdong@gmail.com>
2024-03-18 22:41:42 -0400

    init project

---
+++ TranscriptionExpress/TranscriptionExpress/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranscriptionExpress
+//
+//  Created by Danny on 2024-03-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranscriptionExpress/TranscriptionExpress/TranscriptionExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranscriptionExpressApp.swift
+//  TranscriptionExpress
+//
+//  Created by Danny on 2024-03-18.
+//
+
+import SwiftUI
+
+@main
+struct TranscriptionExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
