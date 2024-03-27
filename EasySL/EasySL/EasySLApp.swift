7dc562570a85236a5f786c698e48d06c858e4152
老房东 <huangdong@gmail.com>
2024-03-27 07:58:29 -0400

    init EasySL prj

---
+++ EasySL/EasySL/EasySLApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySLApp.swift
+//  EasySL
+//
+//  Created by Danny on 2024-03-27.
+//
+
+import SwiftUI
+
+@main
+struct EasySLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
